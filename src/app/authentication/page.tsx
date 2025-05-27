79550c30330306a8a71fc6aa691aa9cd5c84eff6
Daniel Oliveira <danielbarrosdeoliveira@outlook.com>
2025-05-27 11:45:26 -0300

    ✨: add signup form

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+// import { Label } from '@/components/ui/label'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
+import SignUpForm from './components/sign-up-form'
+
+// const loginSchema = z.object({
+//   email: z.string().trim().email({ message: 'Email inválido' }),
+//   password: z
+//     .string()
+//     .min(8, { message: 'Senha deve ter pelo menos 8 caracteres' }),
+// })
+
+const Authentication = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Criar Conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>Faça login para continuar</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2"></CardContent>
+            <CardFooter>
+              <Button>Entrar</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="register">
+          <SignUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
+}
+
+export default Authentication
