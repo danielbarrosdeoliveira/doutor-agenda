2d1bc814c8fa56dba268a957378ea416450c0381
Daniel Oliveira <danielbarrosdeoliveira@outlook.com>
2025-05-30 16:45:06 -0300

    ✨: ensure doctor availability os saved as utc on database

--- src/actions/upsert-doctor/index.ts
+++ src/actions/upsert-doctor/index.ts
@@ -1,5 +1,7 @@
 'use server'
 
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
 import { revalidatePath } from 'next/cache'
 import { headers } from 'next/headers'
 
@@ -10,9 +12,26 @@ import { actionClient } from '@/lib/next-safe-action'
 
 import { upsertDoctorSchema } from './schema'
 
+dayjs.extend(utc)
+
 export const upsertDoctor = actionClient
   .schema(upsertDoctorSchema)
   .action(async ({ parsedInput }) => {
+    const availableFromTime = parsedInput.availableFromTime
+    const availableToTime = parsedInput.availableToTime
+
+    const availableFromTimeUTC = dayjs()
+      .set('hour', parseInt(availableFromTime.split(':')[0]))
+      .set('minute', parseInt(availableFromTime.split(':')[1]))
+      .set('second', parseInt(availableToTime.split(':')[2]))
+      .utc()
+
+    const availableToTimeUTC = dayjs()
+      .set('hour', parseInt(availableToTime.split(':')[0]))
+      .set('minute', parseInt(availableToTime.split(':')[1]))
+      .set('second', parseInt(availableToTime.split(':')[2]))
+      .utc()
+
     const session = await auth.api.getSession({
       headers: await headers()
     })
@@ -29,13 +48,17 @@ export const upsertDoctor = actionClient
       .insert(doctorsTable)
       .values({
         ...parsedInput,
+        availableFromTime: availableFromTimeUTC.format('HH:mm:ss'),
+        availableToTime: availableToTimeUTC.format('HH:mm:ss'),
         id: parsedInput.id,
         clinicId: session?.user.clinic?.id
       })
       .onConflictDoUpdate({
         target: [doctorsTable.id],
         set: {
-          ...parsedInput
+          ...parsedInput,
+          availableFromTime: availableFromTimeUTC.format('HH:mm:ss'),
+          availableToTime: availableToTimeUTC.format('HH:mm:ss')
         }
       })
     revalidatePath('/doctors')
